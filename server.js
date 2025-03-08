d1e626d00efe9a32ac0c70f743f74f2ab876d757
Hari Narayan <harinarayan740@gmail.com>
2025-03-08 23:30:29 +0530

    Update server & vercel config

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-require("dotenv").config();
+// require("dotenv").config();
+// const express = require("express");
+
+// const rootsRoutes = require("./src/routes");
+
+// const app = express();
+// const PORT = process.env.PORT || 3100;
+
+// // Connect to MongoDB
+// require("./src/config/db");
+
+// // Middleware
+// app.use(express.json());
+
+// app.get("/", (req, res) => res.send("Express on Vercel"));
+
+// // Base URL config
+// app.use("/api", rootsRoutes);
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+//   console.log(`Base URL http://localhost:${PORT}/api`);
+// });
+
 const express = require("express");
-
-const rootsRoutes = require("./src/routes");
-
 const app = express();
-const PORT = process.env.PORT || 3100;
-
-// Connect to MongoDB
-require("./src/config/db");
-
-// Middleware
-app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Base URL config
-app.use("/api", rootsRoutes);
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-  console.log(`Base URL http://localhost:${PORT}/api`);
-});
+module.exports = app;
